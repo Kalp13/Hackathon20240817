3ba21704ba673a687472495d9b7aef0439693e54
dalton <contact@dalton.sh>
2024-08-17 16:21:24 +0200

    Tags search fix

--- Frontend/src/routes/product/[[searchCriteria]]/+page.svelte
+++ Frontend/src/routes/product/[[searchCriteria]]/+page.svelte
@@ -1,3 +1,4 @@
+<script lang="ts">
   import { page } from "$app/stores";
   import productService from "$lib/services/productService";
   import {afterNavigate, goto} from '$app/navigation'
@@ -5,23 +6,25 @@
 
   let products: IProductResponse[] = [];
   let tags: ITagResponse[] = [];
-  import {afterNavigate, goto} from '$app/navigation';
-  
-  let products: IProductResponse[] = [];
-  let pageNumber: number = 0;
-  let y: number;
 
-  afterNavigate(async () => {
+  onMount(async () => {
+    let searchCriteria = null;
+    $: searchCriteria = $page.params.searchCriteria;
+
+    let productListRequest: IProductListRequest =  {
+      page : 0,
+      pageSize : 200,
+      search : searchCriteria,
+      tags : [],
+    }
+
     productService.productList.subscribe((value) => {
-      if(value == undefined){
+      if(!value){
         return;
       }
-
-      value.forEach(el =>{
-        products.push(el);
-      });
-      products = products;
-      console.log(products);
+      else{
+        products = value;
+      }
     });
 
     productService.tags.subscribe((value) => {
@@ -49,13 +52,9 @@
 
     console.log(searchTags);
 
-    await fetchProducts();
-  });
-
-  async function fetchProducts(pageNumber:number = 0){
     let productListRequest: IProductListRequest =  {
-      page : pageNumber,
-      pageSize : 10,
+      page : 0,
+      pageSize : 200,
       search : searchCriteria,
       tags : searchTags,
     }
@@ -70,28 +69,11 @@
     });
 
     await productService.getProductList(productListRequest);
+    await productService.getTagsList();
   });
-      tags : []
-    }
-
-    await productService.getProductList(productListRequest); 
-  }
 
   function viewProduct(id: number){
     goto(`/product/productDetail/${id}`);
-  }
-
-  async function loadMore(){
-    pageNumber++;
-    await fetchProducts(pageNumber);
-  }
-
-  async function checkScrollPosition(){
-    const scrolledTo = window.scrollY + window.innerHeight
-    const isReachBottom = document.body.scrollHeight === scrolledTo
-
-    if (isReachBottom) 
-      await loadMore();
   }
 
   $: searchCriteria = $page.params.searchCriteria;
@@ -102,29 +84,30 @@
   <meta name="description" content="About this app" />
 </svelte:head>
 
-<svelte:window on:scroll={checkScrollPosition} bind:scrollY={y} />
-
-  <div class="grid grid-cols-1 xl:grid-cols-4 gap-4 px-1 py-4">
-    {#each products as product, index}
-    <div class="flex flex-col rounded-2xl w-72 bg-[#ffffff] shadow-xl p-4 h-96">
-        <button class="h-full" on:click={()=> viewProduct(product.id)}>
-        <figure class="flex justify-center items-center rounded-2xl">
-          <img
-            src="{product.primaryImage}"
-            alt="Card Preview"
-            class="size-32"
-          />
-        </figure>
-        <div class="flex flex-col pt-5">
-          <div class="text-base font-bold pb-6">{product.name}</div>
-          <div class="flex justify-start pt-6 text-xl text-sky-600">
-            <p>R{product.price}</p>
-          </div>
-        </div>
-      </button>
-    </div>
-  {/each}
+{#if !products}
+  <div class="w-full justify-center">
+    <p>Loading...</p>
   </div>
-  {/if}
-
-</div>
+{:else}
+  <div class="grid grid-cols-1 xl:grid-cols-4 gap-4 px-1 py-4">
+    {#each products as product}
+      <div class="flex flex-col rounded-2xl w-72 bg-[#ffffff] shadow-xl p-4 h-96">
+        <button class="h-full" on:click={()=> viewProduct(product.id)}>
+          <figure class="flex justify-center items-center rounded-2xl">
+            <img
+                    src="{product.primaryImage}"
+                    alt="Card Preview"
+                    class="size-32"
+            />
+          </figure>
+          <div class="flex flex-col pt-5">
+            <div class="text-base font-bold pb-6">{product.name}</div>
+            <div class="flex justify-start pt-6 text-xl text-sky-600">
+              <p>R{product.price}</p>
+            </div>
+          </div>
+        </button>
+      </div>
+    {/each}
+  </div>
+{/if}
