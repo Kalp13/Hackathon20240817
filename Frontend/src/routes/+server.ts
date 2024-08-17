50863871ef8d44b44c06595b4b2c8256b0d1bdc0
Rudolph Kalp <rudolphk@lightstone.co.za>
2024-08-17 10:51:11 +0200

    Added remaining server calls for backend

--- Frontend/src/routes/+server.ts
+++ Frontend/src/routes/+server.ts
@@ -5,9 +5,22 @@ export const GET: RequestHandler = async () => {
 };
 
 var baseUrl = "https://hackathonappservice20240817.azurewebsites.net/";
+
+//Products
 var productSingleUrl = baseUrl + "products/single"; //GET
 var productsList = baseUrl + "products/list"; //POST
 var productsCreate = baseUrl + "products/create"; //PUT
 var productsUpdate = baseUrl + "products/update"; //PATCH
 var productsDelete = baseUrl + "products/delete"; //DELETE
 var productsSearch = baseUrl + "products/search"; //POST
+
+//Purchasing
+var purchaseCreate = baseUrl + "purchase/create"; //PUT
+var purchaseList = baseUrl + "purchase/list"; //GET
+
+//Tags
+var tagsList = baseUrl + "tags/list"; //GET
+
+//Users
+var usersLogin = baseUrl + "users/login"; //POST
+var usersList = baseUrl + "users/list"; //GET
