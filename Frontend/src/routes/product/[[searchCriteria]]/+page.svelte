54e0dbadabc9f90eb985344a8717b87f558f8443
Nantus Groenewald <nantusg@agilebridge.co.za>
2024-08-17 13:28:14 +0200

    Show details

--- Frontend/src/routes/product/[[searchCriteria]]/+page.svelte
+++ Frontend/src/routes/product/[[searchCriteria]]/+page.svelte
@@ -30,31 +30,20 @@
   <title>About</title>
   <meta name="description" content="About this app" />
 </svelte:head>
-
-<div class="text-column">
-  {#if searchCriteria}
-    <p>
-      {searchCriteria}
-    </p>
-  {:else}
-    <p>Loading...</p>
-  {/if}
-</div>
-
   <div class="grid grid-cols-1 xl:grid-cols-4 gap-4 px-1 py-4">
-    {#each Array(products) as product, index}
+    {#each products as product, index}
       <div class="flex flex-col rounded-2xl w-72 bg-[#ffffff] shadow-xl">
         <figure class="flex justify-center items-center rounded-2xl">
           <img
-            src="https://tailwind-generator.b-cdn.net/images/card-generator/tailwind-card-generator-card-preview.png"
+            src="{product.primaryImage}"
             alt="Card Preview"
             class="rounded-t-2xl"
           />
         </figure>
         <div class="flex flex-col p-8">
-          <div class="text-2xl font-bold text-[#374151] pb-6">{product}</div>
+          <div class="text-2xl font-bold text-[#374151] pb-6">{product.name}</div>
           <div class="flex justify-start pt-6">
-            <p>Price</p>
+            <p>R{product.price}</p>
           </div>
         </div>
       </div>
