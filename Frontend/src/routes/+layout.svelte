b949ff93f1ffa73859af39c8ed708ff5de31ca51
Nantus Groenewald <nantusg@agilebridge.co.za>
2024-08-17 11:50:44 +0200

    Product Page WIP

--- Frontend/src/routes/+layout.svelte
+++ Frontend/src/routes/+layout.svelte
@@ -16,9 +16,6 @@
 		<slot />
 	</main>
 
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
 </div>
 
 <style>
@@ -34,7 +31,6 @@
 		flex-direction: column;
 		padding: 1rem;
 		width: 100%;
-		max-width: 64rem;
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
--- Frontend/src/routes/product/[searchCriteria]/+page.svelte
+++ Frontend/src/routes/product/[searchCriteria]/+page.svelte
@@ -1,6 +1,13 @@
+<script lang="ts">
+  import { ProductService } from "$lib/services/productService";
+
+	ProductService
+  let counter = 20;
+</script>
+
 <svelte:head>
-	<title>About</title>
-	<meta name="description" content="About this app" />
+  <title>About</title>
+  <meta name="description" content="About this app" />
 </svelte:head>
 
 <script lang="ts">
@@ -12,6 +19,20 @@
     $: searchCriteria = $page.params.searchCriteria;
 </script>
 
+<div class="grid grid-cols-1 xl:grid-cols-4 gap-4 px-1 py-4">
+  {#each Array(counter) as _, index}
+  <div class="flex flex-col rounded-2xl w-72 bg-[#ffffff] shadow-xl">
+    <figure class="flex justify-center items-center rounded-2xl">
+        <img src="https://tailwind-generator.b-cdn.net/images/card-generator/tailwind-card-generator-card-preview.png" alt="Card Preview" class="rounded-t-2xl">
+    </figure>
+    <div class="flex flex-col p-8">
+        <div class="text-2xl font-bold   text-[#374151] pb-6">Product Name</div>
+        <div class="flex justify-start pt-6">
+            <p>Price</p>
+        </div>
+    </div>
+</div>
+  {/each}
 <div class="text-column">
     {#if searchCriteria}
         <p>
