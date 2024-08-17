a143459b8d315e4f402ea51e8cbf110a213a4b27
Nantus Groenewald <nantusg@agilebridge.co.za>
2024-08-17 13:08:26 +0200

    WIP

--- Frontend/src/routes/product/[[searchCriteria]]/+page.svelte
+++ Frontend/src/routes/product/[[searchCriteria]]/+page.svelte
@@ -1,12 +1,27 @@
 <script lang="ts">
   import { page } from "$app/stores";
+  import productService from "$lib/services/productService";
   import { onMount } from "svelte";
+  
 
-  import { ProductService } from "$lib/services/productService";
+  let productListRequest: IProductListRequest =  {
+    page : 0,
+    pageSize : 200,
+    search : "",
+    tags : [],
+  }
 
-  let counter = 20;
+  let products: IProductResponse[] = [];
 
-  let searchCriteria: string;
+
+  onMount(async () => {
+    productService.productList.subscribe((value) => {
+	  console.log(value);
+    products = value;
+    });
+
+    await productService.getProductList(productListRequest);  
+  });
 
   $: searchCriteria = $page.params.searchCriteria;
 </script>
@@ -27,7 +42,7 @@
 </div>
 
   <div class="grid grid-cols-1 xl:grid-cols-4 gap-4 px-1 py-4">
-    {#each Array(counter) as _, index}
+    {#each Array(products) as product, index}
       <div class="flex flex-col rounded-2xl w-72 bg-[#ffffff] shadow-xl">
         <figure class="flex justify-center items-center rounded-2xl">
           <img
@@ -37,7 +52,7 @@
           />
         </figure>
         <div class="flex flex-col p-8">
-          <div class="text-2xl font-bold text-[#374151] pb-6">Product Name</div>
+          <div class="text-2xl font-bold text-[#374151] pb-6">{product}</div>
           <div class="flex justify-start pt-6">
             <p>Price</p>
           </div>
