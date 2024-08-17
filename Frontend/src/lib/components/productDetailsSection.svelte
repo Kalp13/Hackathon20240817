7d5a228c4252004722daa9920ddf73b820db1d19
Etienne <etiennet@lightstone.co.za>
2024-08-17 12:01:21 +0200

    checkin

--- Frontend/src/lib/components/productDetailsSection.svelte
+++ Frontend/src/lib/components/productDetailsSection.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
 
-	const images:string[] = getImages()
-
+	export let product: IProductResponse;
+		product = {
+		description : "1.7L, 2200W, Auto Shut Off, Boil Dry Protection, Cordless, Water Level Indicator, 360 Degree Base, Removable Limescale Filter, Concealed Element, Rapid Boil, Stainless Steel, Black, 2 Year Warranty",
+		price : 1000,
+		id : 1,
+		name : "Product 1",
+		primaryImage:"https://th.bing.com/th/id/R.e95c538294b6b75f713c050ce475ed3d?rik=OaCLgLeDFQnDEg&pid=ImgRaw&r=0",
+		images:getImages(),
+		tags : ['tag1','tag2','tag3']
+	}
 	
+	const images:string[] = getImages();
+
+
 	function getImages():string[]{//Replace with call to api
 		return ['https://th.bing.com/th/id/R.e95c538294b6b75f713c050ce475ed3d?rik=OaCLgLeDFQnDEg&pid=ImgRaw&r=0','https://th.bing.com/th/id/R.62325205054ee42cbd441c7036a7e3ec?rik=RHdJrVUP%2b%2b8klA&pid=ImgRaw&r=0','https://th.bing.com/th/id/R.2e428e8ae830e4015f0df533b8f006e1?rik=zskWlzdQaXpE1g&riu=http%3a%2f%2fwww.dumpaday.com%2fwp-content%2fuploads%2f2016%2f02%2frandom-pictures-1.jpg&ehk=xuubRylr%2bQ819mR1Fmu%2bbeB0Nbh5KEQ37YIe0L0JaK4%3d&risl=&pid=ImgRaw&r=0','https://i.imgur.com/PrSylav.jpg']
 	}
@@ -23,22 +34,43 @@
 </svelte:head>
 
 <div class="grid grid-cols-2 gap-4 mt-14">
-	<div class="...">
+	<div class="... bg-white p-2 rounded-lg">
 		<div class="col-span-12 max-w-96 mx-1 justify-center">
-			<img id="mainImage" class="w-96 h-80" src="{images[0]}" alt="Random Image" />
+			<img id="mainImage" class="w-96 h-80" src="{product.primaryImage}" alt="Random Image" />
 		</div>
 	
 		<div class="col-span-12 flex flex-row">
-		{#each getImages().map((image, index) => ({ image, index })) as { image, index }}
+		{#each product.images.map((image, index) => ({ image, index })) as { image, index }}
 			<div class="col-span-2 max-w-20 mx-1 my-1" >
 				<img id={index.toString()} class="w-20 h-20" src={image} on:click={handleClick} alt="Random Image" />
 			</div>
 		{/each}
 		</div>
 	</div>
-	<div class="...">
-		<h1 class="text-2xl font-bold">{}</h1>
-		<!-- <p class="text-lg">{description}</p>
-		<p class="text-lg">{price}</p> -->
+	<div class="... bg-white p-2 rounded-lg">
+		<h1 class="text-2xl font-bold">{product.name}</h1>
+		<p class="text-lg">{product.description}</p>
+		<div class="flex flex-wrap">
+			{#each product.tags.map((tag)=>({tag})) as {tag}}
+				<p class="fancy-tag">{tag}</p>
+				
+			{/each}
+		</div>
+		<p class="text-lg">ZAR {product.price}</p>
+
+		<button class="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+			Add to Cart</button>
 	</div>
 </div>
+
+<style>
+	.fancy-tag{
+		background-color: #a3a2a2;
+		width: 6rem;
+		border-radius: 5px;
+		color: white;
+		padding: 5px;
+		margin: 5px;
+		display: inline-block;
+	}
+</style>
